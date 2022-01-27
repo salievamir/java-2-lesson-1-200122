a07c206773d2ada1bceb1d85cfed9cd05a766dfb
Amir <salievamir@gmail.com>
2022-01-27 20:55:31 +0500

    Initial commit

---
+++ src/main/java/com/geekbrains/person/Person.java
@@ -0,0 +1,17 @@
+package com.geekbrains.person;
+
+public abstract class Person {
+    private long cash;
+
+    public long getCash() {
+        return cash;
+    }
+
+    public void setCash(long cash) {
+        this.cash = cash;
+    }
+
+    public static void favoriteSeller(String fullName) {
+
+    }
+}
